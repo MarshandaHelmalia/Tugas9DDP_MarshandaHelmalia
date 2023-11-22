6631326285063a28383ca9f4276093eb17f3ba25
MarshandaHelmalia <146051076+MarshandaHelmalia@users.noreply.github.com>
2023-11-22 15:21:13 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,9 @@
+#deklarasi
+def profil(nama, alamat, gender, umur, hoby):
+    print("Nama saya adalah", nama)
+    print("Saya tinggal di", alamat)
+    print("Saya adalah", gender)
+    print("Umur saya adalah", umur)
+    print("Hoby saya adalah", hoby)
+#panggil
+profil("Marshanda", "Depok", "Perempuan", "20", "Menggambar")
---
+++ soal3.py
@@ -0,0 +1,7 @@
+#deklarasi
+def bilangan_ganjil(a):
+    for b in range(1, a+1, 2):
+        print(b)
+
+#panggil
+bilangan_ganjil (100)
